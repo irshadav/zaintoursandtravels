28e7fff5145671491b0d5c57ee993d494305e648
Irshad Ahamed <irshad.cs@gmail.com>
2014-10-06 09:32:36 +0530

    Enquiry Form Generated

--- contacts.php
+++ contacts.php
@@ -13,7 +13,6 @@
     <ul class="breadcrumbs">
         <li><a href="/">Home</a></li>
         <li><span>Contact details</span></li>
-
     </ul>
 </div>
 <!-- breadcrumbs -->
@@ -22,55 +21,30 @@
     <div class="row">
 
         <div class="large-12 columns" style="margin:10px auto;">
-            <!-- <h1><a name="aboutus" id="aboutus"></a>About Refco world (Refco Engineers) </h1>           -->
+            
             <div class="row">
-                <div class="large-4 columns ">
-                    <h2>Refco World</h2>
-                    <p>35/109, Refco building,
-                    <br> Thakiya road,
-                    <br>Mahawatha,
-                    <br>Mawanella, 71500
-                    <br>Sri Lanka.
-                    <br>
-                    Phone  <span class="numbers">+ 94 812315803</span>
-                    <br>
-                    Fax  <span class="numbers">+ 94 352247932</span>
-                    </p>
+                <div class="large-8 columns" style="background:#F0FFFF;">
+                <div class="panel"><h1>Request for Information</h1>
+                <h3>Please take a moment to fill the form.</h3></div>
+                
+                    <script type="text/javascript" src="http://form.jotform.me/jsform/42763897069472"></script>
                 </div>
-                <div class="large-4 columns ">
-
-                    <p>
-                    <br><br><br><br>
-                    </p>
-                </div>
-                <div class="large-4 columns panel" style="border:none; background:#FAEBD7;">
+                <div class="large-4 columns">
                     <h1>Office</h1>
                     <p>35/109, Galle Road,
-                    <br>Colombo - 03,                    
+                    <br>Colombo - 03,
                     <br>Sri Lanka.<br>
                     <hr>
                     <h3>For Apartment Bookings</h3>
-                    <span class="numbers red">+ 94 774728640</span>
+                    <span class="numbers">+ 94 774728640</span>
                     <hr>
                     <h3>For Tour Arrangments</h3>
-                    <span class="numbers red">+ 94 774728640</span>
+                    <span class="numbers">+ 94 774728640</span>
                 </div>
             </div>
-            <hr>
-            <div class="row ">
-                <div class="large-6 columns ">
-                    <h3>Manager / Principal Consultant</h3>
-                    <p>Mr. Sarath Gunasekara<br>
-                    <span class="numbers">+ 94 774728640</span></p>
-                    <br>
-                    <h3>Manager / Engineer</h3>
-                    <p>Mr. Saroos<br>
-                    <span class="numbers">+94 776645865</span></p>
-                </div>
+            
 
-            </div>
         </div>
-
     </div>
 </div>
 <!-- laundry machines -->
---
+++ enquiry.php
@@ -0,0 +1,132 @@
+<script src="https://static-interlogyllc.netdna-ssl.com/static/prototype.forms.js" type="text/javascript"></script>
+<script src="https://static-interlogyllc.netdna-ssl.com/static/jotform.forms.js?3.2.3887" type="text/javascript"></script>
+<script src="https://js.jotform.com/vendor/postMessage.min.js" type="text/javascript"></script>
+<script src="https://js.jotform.com/WidgetsServer.js" type="text/javascript"></script>
+<script type="text/javascript">
+   JotForm.init(function(){
+      $('input_3').hint('ex: myname@example.com');
+   });
+</script>
+<link href="https://static-interlogyllc.netdna-ssl.com/static/formCss.css?3.2.3887" rel="stylesheet" type="text/css" />
+<link type="text/css" media="print" rel="stylesheet" href="https://static-interlogyllc.netdna-ssl.com/css/printForm.css?3.2.3887" />
+<style type="text/css">
+    .form-label-left{
+        width:150px !important;
+    }
+    .form-line{
+        padding-top:12px;
+        padding-bottom:12px;
+    }
+    .form-label-right{
+        width:150px !important;
+    }
+    .form-all{
+        width:690px;
+        background: #E8E8E8;
+        color:#1F1F1F !important;
+        font-family:'Helvetica';
+        font-size:18px;
+    }
+    .form-radio-item label, .form-checkbox-item label, .form-grading-label, .form-header{
+        color: #1F1F1F;
+    }
+
+</style>
+
+<link type="text/css" rel="stylesheet" href="https://static-interlogyllc.netdna-ssl.com/css/responsive.css?3.2.3887" />
+<link type="text/css" rel="stylesheet" href="https://secure.jotform.me/css/styles/buttons/form-submit-button-blue.css?3.2.3887"/>
+<form class="jotform-form" action="https://submit.jotform.me/submit/42753721633454/" method="post" name="form_42753721633454" id="42753721633454" accept-charset="utf-8">
+  <input type="hidden" name="formID" value="42753721633454" />
+  <div class="form-all">
+    <ul class="form-section">
+      <li class="form-line" data-type="control_textbox" id="id_7">
+        <label class="form-label form-label-left form-label-auto" id="label_7" for="input_7"> Name </label>
+        <div id="cid_7" class="form-input">
+          <input type="text" class=" form-textbox" data-type="input-textbox" id="input_7" name="q7_name" size="20" value="" />
+        </div>
+      </li>
+      <li class="form-line" data-type="control_email" id="id_3">
+        <label class="form-label form-label-left form-label-auto" id="label_3" for="input_3"> E-mail </label>
+        <div id="cid_3" class="form-input">
+          <input type="email" class=" form-textbox validate[Email]" id="input_3" name="q3_email3" size="30" value="" />
+        </div>
+      </li>
+      <li class="form-line" data-type="control_widget" id="id_9">
+        <label class="form-label form-label-left" id="label_9" for="input_9"> Select Country </label>
+        <div id="cid_9" class="form-input">
+          <div style="width:100%; text-align:Left;">
+            <iframe onload="widgetFrameLoaded(9)" frameborder="0" scrolling="no" class="custom-field-frame" id="customFieldFrame_9" src="" style="border:none;width:365px; height: 50px;">
+            </iframe>
+            <div>
+              <input id="input_9" class="form-hidden form-widget" type="hidden" name="q9_selectCountry" value="">
+            </div>
+            <script>
+            setTimeout(function()
+{
+  document.getElementById("customFieldFrame_9").src = "http://widgets.jotform.io/pickers/?pickerType=countries&country=%3Cempty%3E&qid=9&ref=" + encodeURIComponent(window.location.protocol + "//" + window.location.host);
+}, 5);
+var _JCFClientID = 9
+            </script>
+          </div>
+        </div>
+      </li>
+      <li class="form-line" data-type="control_phone" id="id_5">
+        <label class="form-label form-label-left form-label-auto" id="label_5" for="input_5"> Phone Number </label>
+        <div id="cid_5" class="form-input">
+          <span class="form-sub-label-container">
+            <input class="form-textbox" type="tel" name="q5_phoneNumber5[area]" id="input_5_area" size="3">
+            -
+            <label class="form-sub-label" for="input_5_area" id="sublabel_area"> Area Code </label>
+          </span>
+          <span class="form-sub-label-container">
+            <input class="form-textbox" type="tel" name="q5_phoneNumber5[phone]" id="input_5_phone" size="8">
+            <label class="form-sub-label" for="input_5_phone" id="sublabel_phone"> Phone Number </label>
+          </span>
+        </div>
+      </li>
+      <li class="form-line" data-type="control_widget" id="id_6">
+        <label class="form-label form-label-left form-label-auto" id="label_6" for="input_6"> Interested in </label>
+        <div id="cid_6" class="form-input">
+          <div style="width:100%; text-align:Left;">
+            <iframe onload="widgetFrameLoaded(6)" frameborder="0" scrolling="no" class="custom-field-frame" id="customFieldFrame_6" src="" style="border:none;width:350px; height: 200px;">
+            </iframe>
+            <div>
+              <input id="input_6" class="form-hidden form-widget" type="hidden" name="q6_interestedIn" value="">
+            </div>
+            <script>
+            setTimeout(function()
+{
+  document.getElementById("customFieldFrame_6").src = "http://widgets.jotform.io/checklist/?items=Travel%20Sri%20Lanka%0ARent%20our%20Apartment%0ABoth%20above&qid=6&ref=" + encodeURIComponent(window.location.protocol + "//" + window.location.host);
+}, 5);
+var _JCFClientID = 6
+            </script>
+          </div>
+        </div>
+      </li>
+      <li class="form-line" data-type="control_textarea" id="id_8">
+        <label class="form-label form-label-left form-label-auto" id="label_8" for="input_8"> Requiements </label>
+        <div id="cid_8" class="form-input">
+          <textarea id="input_8" class="form-textarea" name="q8_requiements" cols="40" rows="6"></textarea>
+        </div>
+      </li>
+      <li class="form-line" data-type="control_button" id="id_2">
+        <div id="cid_2" class="form-input-wide">
+          <div style="margin-left:156px" class="form-buttons-wrapper">
+            <button id="input_2" type="submit" class="form-submit-button form-submit-button-blue">
+              Submit
+            </button>
+          </div>
+        </div>
+      </li>
+      <li style="display:none">
+        Should be Empty:
+        <input type="text" name="website" value="" />
+      </li>
+    </ul>
+  </div>
+  <input type="hidden" id="simple_spc" name="simple_spc" value="42753721633454" />
+  <script type="text/javascript">
+  document.getElementById("si" + "mple" + "_spc").value = "42753721633454-42753721633454";
+  </script>
+  <script src="https://secure.jotform.me/js/widgetResizer.js?REV=3.2.3887" type="text/javascript"></script>
+</form>
